297b8cef23389b8e110c64f0ecfd51084e2a7716
Hao Yuan <haomichaelyuan@gmail.com>
2022-09-22 19:12:07 +1000

    commit tree 2b9215255ad141f31ba1933fafa36da74a2edd7f

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+ function upperCaseCustomerName() {
+    return customerName.toUpperCase();
+}
+upperCaseCustomerName()
+console.log(upperCaseCustomerName())
+function setBestCustomer(){
+    bestCustomer="not bob"
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer="abc"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="aaa"
+}
